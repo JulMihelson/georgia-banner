e81e5937224cfc7bbf793adb6a97346153b6176f
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2024-03-05 11:36:44 +0200

    upd

--- src/interfaces/navigation.interface..ts
+++ src/interfaces/navigation.interface..ts
@@ -1,44 +1,39 @@
-interface BaseNavigationItem {
-  name: string;
-  route: string;
-}
-export interface INavigation extends BaseNavigationItem {
-  children?: BaseNavigationItem[];
-}
+// interface BaseNavigationItem {
+//   name: string;
+//   route: string;
+// }
+// export interface INavigation extends BaseNavigationItem {
+//   children?: BaseNavigationItem[];
+// }
 
-interface MediaLinkIcon {
-  src: string;
-  width?: number;
-  height?: number;
-}
-export interface IMediaLink extends BaseMediaLinkItem {}
+// interface MediaLinkIcon {
+//   src: string;
+//   width?: number;
+//   height?: number;
+// }
+// export interface IMediaLink extends BaseMediaLinkItem {}
 
-interface BaseMediaLinkItem {
-  name: string;
-  url: string;
-  icon: string;
-}
-interface BreadcrumbItem {
-  text: string;
-  href: string;
-  "aria-current"?: string;
-}
+// interface BaseMediaLinkItem {
+//   name: string;
+//   url: string;
+//   icon: string;
+// }
 
-export interface TransitionAnimation {
-  name: string;
-  delay?: number | string;
-  duration?: number | string;
-  easing?: string;
-  fillMode?: string;
-  direction?: string;
-}
+// export interface TransitionAnimation {
+//   name: string;
+//   delay?: number | string;
+//   duration?: number | string;
+//   easing?: string;
+//   fillMode?: string;
+//   direction?: string;
+// }
 
-export interface TransitionAnimationPair {
-  old: TransitionAnimation | TransitionAnimation[];
-  new: TransitionAnimation | TransitionAnimation[];
-}
+// export interface TransitionAnimationPair {
+//   old: TransitionAnimation | TransitionAnimation[];
+//   new: TransitionAnimation | TransitionAnimation[];
+// }
 
-export interface TransitionDirectionalAnimations {
-  forwards: TransitionAnimationPair;
-  backwards: TransitionAnimationPair;
-}
+// export interface TransitionDirectionalAnimations {
+//   forwards: TransitionAnimationPair;
+//   backwards: TransitionAnimationPair;
+// }
